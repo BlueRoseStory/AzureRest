0de6c9e39eb4c87a6b3b2f02aeab211cd431b721
BlueRose Story <scottb@tsbrooks.com>
2020-12-24 18:43:17 -0500

    All methods created, need tested

---
+++ contents-id-delete/index.js
@@ -0,0 +1,33 @@
+const shared = require('../shared');
+const cosmos = require('@azure/cosmos');
+const cosmosConnect = process.env.CosmosConnectionString;
+const { CosmosClient } = cosmos;
+
+const client = new CosmosClient(cosmosConnect);
+
+const contents =
+    client.database(shared.cosmos_database)
+        .container(shared.cosmos_container_contents);
+
+module.exports = async function (context, req, item) {
+
+    let res = { body: 'unknown', status: 404 }
+
+    if (req.headers[shared.ownerKey]) {
+        if (item) {
+
+            await contents
+                .item(item.id, req.headers[shared.ownerKey])
+                .delete();
+
+            res.body = undefined;
+            res.status = 204;
+        } else {
+            res.body = `Item: ${context.bindingData.itemId} not found for owner: ${req.headers[shared.ownerKey]}`;
+        }
+    } else {
+        res.body = shared.ownerKey + ' header not found';
+    }
+
+    context.res = res;
+}
